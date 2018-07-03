a5ac27dcd0471305f371f14db0838a50d5e2e533
skillverse <skillverseit@gmail.com>
2018-07-03 19:37:43 +0530

    small change

--- JavaScriptAnimations/script.js
+++ JavaScriptAnimations/script.js
@@ -2,7 +2,6 @@ var slidetime=4000;
 
 function changePicture(){
     var arr=["images/01.jpg","images/02.jpg","images/03.jpg","images/01.jpg","images/02.jpg","images/03.jpg"];
-   
     var image= document.getElementById('gallery');
     src=image.src;
     arr.forEach(img => {
