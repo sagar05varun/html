5af49dfe279bba738c5e501f76a0c86c3b070c74
skillverse <skillverseit@gmail.com>
2018-07-03 19:35:41 +0530

    javascript carousal

---
+++ JavaScriptAnimations/script.js
@@ -0,0 +1,24 @@
+var slidetime=4000;
+
+function changePicture(){
+    var arr=["images/01.jpg","images/02.jpg","images/03.jpg","images/01.jpg","images/02.jpg","images/03.jpg"];
+   
+    var image= document.getElementById('gallery');
+    src=image.src;
+    arr.forEach(img => {
+        if(src.search(img)>1){
+            index=arr.indexOf(img);
+            if(index==length-1){
+                image.src=arr[0];
+            }
+            else{
+                image.src=arr[index+1];
+                image.className="animated fadeIn";
+            }
+        }
+    });
+}
+
+function startslides(){
+    setInterval(changePicture,slidetime);
+}
